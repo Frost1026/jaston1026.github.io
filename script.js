3078bff4d48d4e4ec2cebab823095a745fd5e492
FrostedPhoenix <yizhong1027@gmail.com>
2020-11-28 10:12:30 +0800

    New Web

---
+++ script.js
@@ -0,0 +1,23 @@
+var synth = new Tone.Synth().toMaster();
+
+var pad = document.getElementById('pad')
+var label = document.getElementById('label')
+
+var mouseCoord = function(event){
+  var x = event.pageX
+  synth.triggerAttack(x)
+  label.innerHTML = Math.round(x) + 'Hz'
+}
+
+function held(event) {
+  mouseCoord
+}
+
+function release(event) {
+  synth.triggerRelease()
+  label.innerHTML = 'CLICK / DRAG'
+}
+
+pad.addEventListener('pointerdown', held)
+pad.addEventListener('pointerup', release)
+pad.addEventListener('pointermove', mouseCoord)
