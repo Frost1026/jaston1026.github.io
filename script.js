c729c270662a4e5ef7719d924aa302e585004c1c
FrostedPhoenix <yizhong1027@gmail.com>
2020-11-28 10:33:51 +0800

    semicolon

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 var synth = new Tone.Synth().toMaster();
-var dragging
+var dragging;
 
-var pad = document.getElementById('pad')
-var label = document.getElementById('label')
+var pad = document.getElementById('pad');
+var label = document.getElementById('label');
 
 function held(event) {
-  dragging = true
+  dragging = true;
 }
 
 function release(event) {
-  dragging = false
-  synth.triggerRelease()
-  label.innerHTML = 'CLICK / DRAG'
+  dragging = false;
+  synth.triggerRelease();
+  label.innerHTML = 'CLICK / DRAG';
 }
 
 function moving(event) {
   if (dragging) {
-      var x = event.pageX
-      synth.setNote(x)
-      label.innerHTML = Math.round(x) + 'Hz'
+      var x = event.pageX;
+      synth.setNote(x);
+      label.innerHTML = Math.round(x) + 'Hz';
   }
 }
 
-pad.addEventListener('pointerdown', held)
-pad.addEventListener('pointerup', release)
-pad.addEventListener('pointermove', moving)
+pad.addEventListener('pointerdown', held);
+pad.addEventListener('pointerup', release);
+pad.addEventListener('pointermove', moving);
